d671da3e068a23ba5c93d3664f1bf1c2f6eb1aa6
Balaji Venkatesan <balaji.venkatesan@digital.ai>
2023-01-16 15:00:01 +0530

    S-89329 Added Releases Api

--- tests/test_release_api.py
+++ tests/test_release_api.py
@@ -1,11 +1,16 @@
-import os
+import traceback
 
-from digitalai.release.api import ReleaseConnection
+from digitalai.release.api.v1 import ReleaseServerClient, ReleasesApi
 
 if __name__ == '__main__':
 
-    release = ReleaseConnection(url='http://localhost:5516', username='admin', password='admin')
-    conn_result = release.test_connection()
-    print(f"Connection Result is : {conn_result}")
+    try:
+        release_client = ReleaseServerClient(url='http://localhost:5516', username='admin', password='admin')
+        conn_result = release_client.test_connection()
+        print(f"Connection Result is : {conn_result}")
 
-
+        releases_api = ReleasesApi(release_client)
+        variables = releases_api.releases_release_id_variables_get('Release9e06a6b933f4409e81c12ec62e82216e')
+        print(variables)
+    except:
+        traceback.print_exc()
