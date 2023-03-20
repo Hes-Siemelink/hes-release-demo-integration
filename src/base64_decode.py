d03cf03fccf07d9bafea61c4b107b84fb3b73a81
Balaji Venkatesan <balaji.venkatesan@digital.ai>
2023-03-20 14:42:45 +0530

    For Release API Methods Test

--- src/base64_decode.py
+++ src/base64_decode.py
@@ -1,5 +1,8 @@
 import logging
 import requests
+from digitalai.release.api.v1 import Configuration, ApiClient
+from digitalai.release.api.v1.api.configuration_api import ConfigurationApi
+from digitalai.release.api.v1.api.release_api import ReleaseApi
 from digitalai.release.integration import BaseTask
 
 logger = logging.getLogger('Digitalai')
@@ -22,6 +25,30 @@ class Base64Decode(BaseTask):
             if 'Incorrect Base64 data' in response.text:
                 raise ValueError(response.text)
             self.textValue = response.text
+
+            # For Release API testing
+
+            release_server_url = self.get_release_server_url()
+            if not release_server_url:
+                release_server_url = "http://host.docker.internal:5516"
+
+            task_user = self.get_task_user();
+            if not task_user.username:
+                task_user.username = 'admin'
+                task_user.password = 'admin'
+
+            configuration = Configuration(host=release_server_url, username=task_user.username, password=task_user.password)
+            api_client = ApiClient(configuration)
+
+            configuration_api = ConfigurationApi(api_client)
+            response_api = configuration_api.get_global_variables()
+            print(f"get_global_variables : {response_api}\n")
+
+            release_api = ReleaseApi(api_client)
+            response_api = release_api.get_releases(depth=1, page=0, results_per_page=1)
+            print(f"get_releases : {response_api}\n")
+
+
         except Exception as e:
             logger.error("Unexpected error occurred.", exc_info=True)
             self.set_exit_code(1)
