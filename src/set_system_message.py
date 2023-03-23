bab9595f3de512d0e7c98a34c40d2e0a05ceeedc
Hes Siemelink <>
2023-03-23 15:47:45 +0100

    Code cleanup

--- src/set_system_message.py
+++ src/set_system_message.py
@@ -1,7 +1,6 @@
 import logging
 
 from digitalai.release.integration import BaseTask
-from digitalai.release.v1 import Configuration, ApiClient
 from digitalai.release.v1.api.configuration_api import ConfigurationApi
 from digitalai.release.v1.model.system_message_settings import SystemMessageSettings
 
@@ -18,41 +17,17 @@ class SetSystemMessage(BaseTask):
         self.message = params['message']
 
     def execute(self) -> None:
-        try:
-            # For Release API methods testing
+        configuration_api = ConfigurationApi(self.get_default_api_client())
 
-            # release_server_url = self.get_release_server_url()
-            # if not release_server_url:
-            #     release_server_url = "http://host.docker.internal:5516"
+        system_message = SystemMessageSettings(
+            type='xlrelease.SystemMessageSettings',
+            id='Configuration/settings/SystemMessageSettings',
+            message=self.message,
+            enabled=True,
+            automated=False
+        )
 
-            # task_user = self.get_task_user()
-            # if not task_user.username:
-            #     task_user.username = 'admin'
-            #     task_user.password = 'admin'
+        configuration_api.update_system_message(system_message_settings=system_message)
 
-            # self.add_comment(f"Release URL: `{self.get_release_server_url()}`  \nUser name: `{self.get_task_user()}`")
+        self.add_comment(f"System message updated to \"{self.message}\"")
 
-            configuration = Configuration(
-                # host=self.get_release_server_url(),
-                host="http://host.docker.internal:5516",
-                username=self.get_task_user().username,
-                password=self.get_task_user().password)
-            api_client = ApiClient(configuration)
-            configuration_api = ConfigurationApi(api_client)
-
-            system_message = SystemMessageSettings(
-                message=self.message,
-                type='xlrelease.SystemMessageSettings',
-                id='Configuration/settings/SystemMessageSettings',
-                enabled=True,
-                automated=False
-            )
-
-            configuration_api.update_system_message(system_message_settings=system_message)
-
-            self.add_comment(f"System message updated to {self.message}")
-
-        except Exception as e:
-            logger.error("Unexpected error occurred.", exc_info=True)
-            self.set_exit_code(1)
-            self.set_error_message(str(e))
